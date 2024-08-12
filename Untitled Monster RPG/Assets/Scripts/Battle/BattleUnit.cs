d95f38061f5b2f3e8fddc7e440f92b8c7ae8f099
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-08-11 20:27:24 -0400

    refactor: Update BattleUnit to improve image handling and animation

--- Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
@@ -10,17 +10,39 @@ public class BattleUnit : MonoBehaviour
     [SerializeField] bool isPlayerUnit;
 
     public Monster Monster { get; set; }
+    Image image;
+    Vector3 originalPos;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        originalPos = image.transform.localPosition;
+    }
 
     public void Setup()
     {
         Monster = new Monster(_base, level);
         if (isPlayerUnit)
         {
-            GetComponent<Image>().sprite = Monster.Base.BackSprite;
+            image.sprite = Monster.Base.BackSprite;
         }
         else
         {
-            GetComponent<Image>().sprite = Monster.Base.FrontSprite;
+            image.sprite = Monster.Base.FrontSprite;
+        }
+
+        PlayEnterAnimation();
+    }
+
+    public void PlayEnterAnimation()
+    {
+        if (isPlayerUnit)
+        {
+            image.transform.localPosition = new Vector3(-300f, originalPos.y);
+        }
+        else
+        {
+            image.transform.localPosition = new Vector3(300f, originalPos.y);
         }
     }
 }
