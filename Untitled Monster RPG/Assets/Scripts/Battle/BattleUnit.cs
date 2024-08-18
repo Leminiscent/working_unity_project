d69f969a588cd54230f76c7caaa0b7aae777092b
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-08-18 19:27:20 -0400

    refactor: Update move descriptions, stats, and monster asset metadata

--- Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
@@ -1,8 +1,8 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class BattleUnit : MonoBehaviour
 {
--- Untitled Monster RPG/Assets/Scripts/Character/Character.cs
+++ Untitled Monster RPG/Assets/Scripts/Character/Character.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 public class Character : MonoBehaviour
