479cd32d9598ecbd43c4a2044d54231e06739eac
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-08-24 13:56:41 -0400

    refactor: Add destinationPortal property to Portal.cs for scene switching

--- Untitled Monster RPG/Assets/Scripts/SceneManagement/Portal.cs
+++ Untitled Monster RPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -8,13 +8,14 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] DestinationIdentifier destinationPortal;
     [SerializeField] Transform spawnPoint;
     PlayerController player;
 
     public void OnPlayerTriggered(PlayerController player)
     {
         this.player = player;
-        
+
         StartCoroutine(SwitchScene());
     }
 
@@ -24,7 +25,7 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         GameController.Instance.PauseGame(true);
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
-        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
 
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
         GameController.Instance.PauseGame(false);
@@ -32,4 +33,9 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     }
 
     public Transform SpawnPoint => spawnPoint;
+}
+
+public enum DestinationIdentifier
+{
+    A, B, C, D, E
 }
