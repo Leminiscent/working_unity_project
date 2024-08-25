7b66839c2212bcea694e988087052f8c8095dbc7
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-08-25 19:40:25 -0400

    refactor: Update UI colors and menu functionality

--- Untitled Monster RPG/Assets/Scripts/Inventory/ItemBase.cs
+++ Untitled Monster RPG/Assets/Scripts/Inventory/ItemBase.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBase : MonoBehaviour
+public class ItemBase : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] new string name;
+    [SerializeField] string description;
+    [SerializeField] Sprite icon;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string Name => name;
+    public string Description => description;
+    public Sprite Icon => icon;
 }
---
+++ Untitled Monster RPG/Assets/Scripts/Inventory/RecoveryItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Create new recovery item")]
+public class RecoveryItem : ItemBase
+{
+    [Header("HP")]
+    [SerializeField] int hpAmount;
+    [SerializeField] bool restoreMaxHP;
+
+    [Header("AP")]
+    [SerializeField] int apAmount;
+    [SerializeField] bool restoreMaxAP;
+
+    [Header("Status Conditions")]
+    [SerializeField] ConditionID status;
+    [SerializeField] bool recoverAllStatus;
+
+    [Header("Revive")]
+    [SerializeField] bool revive;
+    [SerializeField] bool maxRevive;
+}
