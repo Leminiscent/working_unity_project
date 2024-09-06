a5e29382faf9099abc42e34d2970ab8f7a8113b1
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-09-06 13:00:40 -0400

    refactor: Update NPCController to handle quest interactions and add questToStart field

---
+++ Untitled Monster RPG/Assets/Scripts/Quests/QuestList.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestList : MonoBehaviour
+{
+    List<Quest> quests = new List<Quest>();
+
+    public void AddQuest(Quest quest)
+    {
+        if (!quests.Contains(quest))
+        {
+            quests.Add(quest);
+        }
+    }
+}
