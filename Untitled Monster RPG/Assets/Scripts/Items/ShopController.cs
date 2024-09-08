8c3cb04d093a834806d46413f3a13588c4a0f621
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-09-08 10:58:37 -0400

    refactor: Update ShopController to include shop menu and selling functionality

--- Untitled Monster RPG/Assets/Scripts/Items/ShopController.cs
+++ Untitled Monster RPG/Assets/Scripts/Items/ShopController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum ShopState { Menu, Buying, Selling, Busy }
 
 public class ShopController : MonoBehaviour
 {
+    [SerializeField] InventoryUI playerInventoryUI;
+    ShopState state = ShopState.Menu;
+
     public static ShopController Instance { get; private set; }
 
     private void Awake()
@@ -13,6 +19,13 @@ public class ShopController : MonoBehaviour
 
     public IEnumerator StartTrading(Merchant merchant)
     {
+        yield return StartMenu();
+    }
+
+    IEnumerator StartMenu()
+    {
+        state = ShopState.Menu;
+
         int selectedChoice = 0;
 
         yield return DialogueManager.Instance.ShowDialogueText("Welcome to my shop! How can I help you today?",
@@ -27,11 +40,27 @@ public class ShopController : MonoBehaviour
         else if (selectedChoice == 1)
         {
             // Sell
+            state = ShopState.Selling;
+            playerInventoryUI.gameObject.SetActive(true);
         }
         else if (selectedChoice == 2)
         {
             // Leave
             yield break;
         }
+    }
+
+    public void HandleUpdate()
+    {
+        if (state == ShopState.Selling)
+        {
+            playerInventoryUI.HandleUpdate(OnBackFromSelling, (selectedItem) => { });
+        }
+    }
+
+    public void OnBackFromSelling()
+    {
+        playerInventoryUI.gameObject.SetActive(false);
+        StartCoroutine(StartMenu());
     }
 }
