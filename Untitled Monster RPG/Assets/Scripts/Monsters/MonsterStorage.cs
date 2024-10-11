73d390640a57198fa80d949c3e7dfea46faa9ee5
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-10-11 12:47:25 -0400

    Refactor MonsterStorage.cs to implement ISavable interface for saving and loading game data

--- Untitled Monster RPG/Assets/Scripts/Monsters/MonsterStorage.cs
+++ Untitled Monster RPG/Assets/Scripts/Monsters/MonsterStorage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterStorage : MonoBehaviour
+public class MonsterStorage : MonoBehaviour, ISavable
 {
     const int numberOfDepots = 10;
     const int numberOfSlots = 48; // 6 * 8
@@ -45,4 +45,64 @@ public class MonsterStorage : MonoBehaviour
     {
         return FindObjectOfType<PlayerController>().GetComponent<MonsterStorage>();
     }
+
+    public object CaptureState()
+    {
+        var saveData = new DepotSaveData()
+        {
+            depotSlots = new List<DepotSlotSaveData>()
+        };
+
+        for (int depotIndex = 0; depotIndex < numberOfDepots; depotIndex++)
+        {
+            for (int slotIndex = 0; slotIndex < numberOfSlots; slotIndex++)
+            {
+                if (depots[depotIndex, slotIndex] != null)
+                {
+                    var depotSlot = new DepotSlotSaveData()
+                    {
+                        monsterData = depots[depotIndex, slotIndex].GetSaveData(),
+                        depotIndex = depotIndex,
+                        slotIndex = slotIndex
+                    };
+
+                    saveData.depotSlots.Add(depotSlot);
+                }
+            }
+        }
+
+        return saveData;
+    }
+
+    public void RestoreState(object state)
+    {
+        var saveData = state as DepotSaveData;
+
+        for (int depotIndex = 0; depotIndex < numberOfDepots; depotIndex++)
+        {
+            for (int slotIndex = 0; slotIndex < numberOfSlots; slotIndex++)
+            {
+                depots[depotIndex, slotIndex] = null;
+            }
+        }
+
+        foreach (var slot in saveData.depotSlots)
+        {
+            depots[slot.depotIndex, slot.slotIndex] = new Monster(slot.monsterData);
+        }
+    }
+}
+
+[System.Serializable]
+public class DepotSaveData
+{
+    public List<DepotSlotSaveData> depotSlots;
+}
+
+[System.Serializable]
+public class DepotSlotSaveData
+{
+    public MonsterSaveData monsterData;
+    public int depotIndex;
+    public int slotIndex;
 }
