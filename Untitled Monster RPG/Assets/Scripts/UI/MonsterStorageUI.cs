c38470d0426c96f5af116269d76b8113f1cd94e4
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-10-11 12:19:07 -0400

    Refactor UI Canvas prefab to activate a game object and update anchor positions

--- Untitled Monster RPG/Assets/Scripts/UI/MonsterStorageUI.cs
+++ Untitled Monster RPG/Assets/Scripts/UI/MonsterStorageUI.cs
@@ -84,11 +84,11 @@ public class MonsterStorageUI : SelectionUI<ImageSlot>
     {
         int prevSelectedDepot = SelectedDepot;
 
-        if (Input.GetButtonDown("Left"))
+        if (Input.GetKeyDown("Q"))
         {
             SelectedDepot = (SelectedDepot - 1) % storage.NumberOfDepots;
         }
-        else if (Input.GetButtonDown("Right"))
+        else if (Input.GetKeyDown("E"))
         {
             SelectedDepot = (SelectedDepot + 1) % storage.NumberOfDepots;
         }
