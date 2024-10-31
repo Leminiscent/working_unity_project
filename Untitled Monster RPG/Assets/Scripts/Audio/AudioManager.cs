515072611b3c3b6c42602cb68d0cbac8e0c73e12
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-10-31 09:50:36 -0400

    Refactor AudioManager to improve variable naming and encapsulation

--- Untitled Monster RPG/Assets/Scripts/Audio/AudioManager.cs
+++ Untitled Monster RPG/Assets/Scripts/Audio/AudioManager.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] List<AudioData> sfxList;
-    [SerializeField] AudioSource musicPlayer;
-    [SerializeField] AudioSource sfxPlayer;
-    [SerializeField] float fadeDuration = 0.75f;
-    AudioClip currentMusic;
-    float originalMusicVolume;
-    Dictionary<AudioID, AudioData> sfxDictionary;
+    [SerializeField] private List<AudioData> _sfxList;
+    [SerializeField] private AudioSource _musicPlayer;
+    [SerializeField] private AudioSource _sfxPlayer;
+    [SerializeField] private float _fadeDuration = 0.75f;
+
+    private AudioClip _currentMusic;
+    private float _originalMusicVolume;
+    private Dictionary<AudioID, AudioData> _sfxDictionary;
 
     public static AudioManager Instance { get; private set; }
 
@@ -23,34 +24,36 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        originalMusicVolume = musicPlayer.volume;
-        sfxDictionary = sfxList.ToDictionary(x => x.id, x => x);
+        _originalMusicVolume = _musicPlayer.volume;
+        _sfxDictionary = _sfxList.ToDictionary(x => x.id, x => x);
     }
 
     public void PlaySFX(AudioClip clip, bool pauseMusic = false)
     {
         if (clip == null) return;
+
         if (pauseMusic)
         {
-            musicPlayer.Pause();
+            _musicPlayer.Pause();
             StartCoroutine(UnpauseMusic(clip.length));
         }
-        sfxPlayer.PlayOneShot(clip);
+
+        _sfxPlayer.PlayOneShot(clip);
     }
 
     public void PlaySFX(AudioID id, bool pauseMusic = false)
     {
-        if (!sfxDictionary.ContainsKey(id)) return;
+        if (!_sfxDictionary.ContainsKey(id)) return;
 
-        var audioData = sfxDictionary[id];
-
+        var audioData = _sfxDictionary[id];
         PlaySFX(audioData.clip, pauseMusic);
     }
 
     public void PlayMusic(AudioClip clip, bool loop = true, bool fade = false)
     {
-        if (clip == null || clip == currentMusic) return;
-        currentMusic = clip;
+        if (clip == null || clip == _currentMusic) return;
+
+        _currentMusic = clip;
         StartCoroutine(PlayMusicAsync(clip, loop, fade));
     }
 
@@ -58,29 +61,37 @@ public class AudioManager : MonoBehaviour
     {
         if (fade)
         {
-            yield return musicPlayer.DOFade(0, fadeDuration).WaitForCompletion();
+            yield return _musicPlayer.DOFade(0, _fadeDuration).WaitForCompletion();
         }
 
-        musicPlayer.clip = clip;
-        musicPlayer.loop = loop;
-        musicPlayer.Play();
+        _musicPlayer.clip = clip;
+        _musicPlayer.loop = loop;
+        _musicPlayer.Play();
 
         if (fade)
         {
-            yield return musicPlayer.DOFade(originalMusicVolume, fadeDuration).WaitForCompletion();
+            yield return _musicPlayer.DOFade(_originalMusicVolume, _fadeDuration).WaitForCompletion();
         }
     }
 
     IEnumerator UnpauseMusic(float delay)
     {
         yield return new WaitForSeconds(delay);
-        musicPlayer.volume = 0;
-        musicPlayer.UnPause();
-        musicPlayer.DOFade(originalMusicVolume, fadeDuration);
+        _musicPlayer.volume = 0;
+        _musicPlayer.UnPause();
+        _musicPlayer.DOFade(_originalMusicVolume, _fadeDuration);
     }
 }
 
-public enum AudioID { UISelect, Hit, Defeat, ExpGain, ItemObtained, MonsterObtained }
+public enum AudioID
+{
+    UISelect,
+    Hit,
+    Defeat,
+    ExpGain,
+    ItemObtained,
+    MonsterObtained
+}
 
 [System.Serializable]
 public class AudioData
