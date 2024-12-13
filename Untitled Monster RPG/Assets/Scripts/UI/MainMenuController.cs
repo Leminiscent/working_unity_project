c5d197335c09efb5afcae5eccd9ee4a6ddcd5b52
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-12-13 12:36:10 -0500

    Refactor MainMenuController script to improve item initialization and restore associated metadata

--- Untitled Monster RPG/Assets/Scripts/UI/MainMenuController.cs
+++ Untitled Monster RPG/Assets/Scripts/UI/MainMenuController.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 using Utils.GenericSelectionUI;
 
 public class MainMenuController : SelectionUI<TextSlot>
 {
     private void Start()
     {
-        SetItems(GetComponentInChildren<TextSlot>().ToList());
+        SetItems(GetComponentsInChildren<TextSlot>().ToList());
     }
 }
