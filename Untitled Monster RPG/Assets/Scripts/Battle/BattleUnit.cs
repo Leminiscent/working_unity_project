330224b50ca56c729b185fcae4b3bc0a77ebf827
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-12-29 19:26:31 -0500

    Refactor BattleUnit to reset image scale and remove redundant SetPosition method

--- Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/BattleUnit.cs
@@ -33,16 +33,11 @@ public class BattleUnit : MonoBehaviour
 
         _originalPos.y = (newSize - originalSize) / 2;
         _image.transform.localPosition = new Vector3(_originalPos.x, _originalPos.y);
+        _image.transform.localScale = Vector3.one;
         _hud.gameObject.SetActive(true);
         _hud.SetData(monster);
         _image.color = _originalColor;
         PlayEnterAnimation();
-    }
-
-    public void SetPosition(Vector3 newPosition)
-    {
-        _image.transform.localPosition = newPosition;
-        _originalPos = newPosition;
     }
 
     public void Clear()
