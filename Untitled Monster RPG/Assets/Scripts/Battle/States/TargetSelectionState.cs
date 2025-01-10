46c88150a18e3cc33ff6d952e00d0282f1d94795
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-01-10 13:20:27 -0500

    Refactor TargetSelectionState to improve target selection logic and UI feedback

--- Untitled Monster RPG/Assets/Scripts/Battle/States/TargetSelectionState.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/States/TargetSelectionState.cs
@@ -5,10 +5,12 @@ using Utils.StateMachine;
 
 public class TargetSelectionState : State<BattleSystem>
 {
-    private int _selectedTargetIndex = 0;
+    private int _selectedTarget = 0;
     private BattleSystem _battleSystem;
 
     public static TargetSelectionState Instance { get; private set; }
+    public bool SelectionMade { get; private set; }
+    public int SelectedTarget => _selectedTarget;
 
     private void Awake()
     {
@@ -22,20 +24,49 @@ public class TargetSelectionState : State<BattleSystem>
         }
     }
 
+    private void UpdateSelectionInUI()
+    {
+        for (int i = 0; i < _battleSystem.EnemyUnits.Count; i++)
+        {
+            _battleSystem.EnemyUnits[i].SetSelected(i == _selectedTarget);
+        }
+    }
+
     public override void Enter(BattleSystem owner)
     {
         _battleSystem = owner;
+        SelectionMade = false;
+        _selectedTarget = 0;
+        UpdateSelectionInUI();
     }
 
     public override void Execute()
     {
+        int prevSelection = _selectedTarget;
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            _selectedTargetIndex++;
+            _selectedTarget++;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            _selectedTargetIndex--;
+            _selectedTarget--;
+        }
+
+        if (_selectedTarget != prevSelection)
+        {
+            UpdateSelectionInUI();
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            SelectionMade = true;
+            _battleSystem.StateMachine.Pop();
+        }
+        else if (Input.GetButtonDown("Back"))
+        {
+            SelectionMade = false;
+            _battleSystem.StateMachine.Pop();
         }
     }
 }
