bb1642cb5fc3865c627b28ece49665c98c00c403
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-01-20 10:06:26 -0500

    Refactor recruitment process to ensure exit animation plays before adding monster to player party

--- Untitled Monster RPG/Assets/Scripts/Battle/States/RecruitmentState.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/States/RecruitmentState.cs
@@ -176,13 +176,12 @@ public class RecruitmentState : State<BattleSystem>
         {
             // Yes
             yield return _dialogueBox.TypeDialogue($"{RecruitTarget.Monster.Base.Name} was recruited!");
-            
+
             RecruitTarget.PlayExitAnimation();
             RecruitTarget.Hud.gameObject.SetActive(false);
-            yield return new WaitForSeconds(0.75f);
-
             _battleSystem.EnemyUnits.Remove(RecruitTarget);
             _battleSystem.PlayerParty.AddMonster(RecruitTarget.Monster);
+            yield return new WaitForSeconds(0.75f);
 
             if (_battleSystem.EnemyUnits.Count == 0)
             {
