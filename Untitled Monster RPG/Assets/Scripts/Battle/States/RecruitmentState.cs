cb08017097379bc44a19e9d1f468c7578076b46c
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-01-22 08:28:10 -0500

    Refactor RecruitmentState to adjust target units dynamically, ensuring valid targets remain for battle actions

--- Untitled Monster RPG/Assets/Scripts/Battle/States/RecruitmentState.cs
+++ Untitled Monster RPG/Assets/Scripts/Battle/States/RecruitmentState.cs
@@ -195,8 +195,15 @@ public class RecruitmentState : State<BattleSystem>
                     defeatedUnitAction.IsValid = false;
                 }
 
-                List<BattleAction> actionsToAdjust = RunTurnState.Instance.BattleActions.Where(a => a.TargetUnits == RecruitTarget).ToList();
-                actionsToAdjust.ForEach(a => a.TargetUnits = _battleSystem.EnemyUnits[Random.Range(0, _battleSystem.EnemyUnits.Count)]);
+                List<BattleAction> actionsToAdjust = RunTurnState.Instance.BattleActions.Where(a => a.TargetUnits.Contains(RecruitTarget)).ToList();
+                foreach (BattleAction a in actionsToAdjust)
+                {
+                    a.TargetUnits.Remove(RecruitTarget);
+                    if (a.TargetUnits.Count == 0)
+                    {
+                        a.TargetUnits.Add(_battleSystem.EnemyUnits[Random.Range(0, _battleSystem.EnemyUnits.Count)]);
+                    }
+                }
             }
         }
         else
