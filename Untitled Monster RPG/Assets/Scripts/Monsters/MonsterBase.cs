1293afbe373e20f319127eb19a6761453e4f51c5
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-02-21 17:00:41 -0500

    Update type effectiveness chart to replace "Dragon" with "Occult" type

--- Untitled Monster RPG/Assets/Scripts/Monsters/MonsterBase.cs
+++ Untitled Monster RPG/Assets/Scripts/Monsters/MonsterBase.cs
@@ -299,7 +299,7 @@ public class TypeChart
 {
     private static float[,] _chart =
     {
-        //             NOR   FIR   WAT   THU   NAT   ICE   FOR   EAR   WIN   DRA   DAR   LIG
+        //             NOR   FIR   WAT   THU   NAT   ICE   FOR   EAR   WIN   OCC   DAR   LIG
         /*Normal*/   { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.5f, 1.0f, 1.0f, 0.5f, 1.0f },
         /*Fire*/     { 1.0f, 0.5f, 0.5f, 1.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 0.5f, 1.0f, 1.0f },
         /*Water*/    { 1.0f, 2.0f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 0.5f, 1.0f, 1.0f },
@@ -309,7 +309,7 @@ public class TypeChart
         /*Force*/    { 2.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f },
         /*Earth*/    { 1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
         /*Wind*/     { 1.0f, 1.0f, 1.0f, 0.5f, 2.0f, 1.0f, 2.0f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f },
-        /*Dragon*/   { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 0.0f },
+        /*Occult*/   { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 0.0f },
         /*Dark*/     { 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
         /*Light*/    { 1.0f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 1.0f, 2.0f, 2.0f, 1.0f }
     };
