9213d96d65087c4f45a966bbbd4875374c7fbec2
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-03-15 12:36:41 -0400

    Rename battle music references and update StoragePartySlotUI to display member roles

--- Untitled RPG/Assets/Scripts/UI/StoragePartySlotUI.cs
+++ Untitled RPG/Assets/Scripts/UI/StoragePartySlotUI.cs
@@ -6,12 +6,14 @@ public class StoragePartySlotUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _nameText;
     [SerializeField] private TextMeshProUGUI _levelText;
+    [SerializeField] private TextMeshProUGUI _roleText;
     [SerializeField] private Image _image;
 
     public void SetData(Battler battler)
     {
         _nameText.text = battler.Base.Name;
-        _levelText.text = "" + battler.Level;
+        _levelText.text = "Lvl " + battler.Level;
+        _roleText.text = battler.IsCommander ? "Commander" : "";
         _image.sprite = battler.Base.Sprite;
         _image.color = new Color(1, 1, 1, 1);
     }
@@ -20,6 +22,7 @@ public class StoragePartySlotUI : MonoBehaviour
     {
         _nameText.text = "";
         _levelText.text = "";
+        _roleText.text = "";
         _image.sprite = null;
         _image.color = new Color(1, 1, 1, 0);
     }
