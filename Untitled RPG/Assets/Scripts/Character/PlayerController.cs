521571527925683335186424ead850e04b088a51
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-03-18 12:36:17 -0400

    Refactor PlayerController for improved clarity and maintainability; replace private field with a property, streamline interaction logic, and enhance state management.

--- Untitled RPG/Assets/Scripts/Character/PlayerController.cs
+++ Untitled RPG/Assets/Scripts/Character/PlayerController.cs
@@ -3,22 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour, ISavable
 {
-    [SerializeField] private string _name;
+    [field: SerializeField, FormerlySerializedAs("_name")] public string Name { get; private set; }
 
     private Battler _playerBattler;
     private Vector2 _input;
-    private Character _character;
-    private DeputyController _deputy;
     private IPlayerTriggerable _currentlyInTrigger;
     private bool _isInteracting;
 
     public static PlayerController Instance { get; private set; }
-    public string Name => _name;
-    public Character Character => _character;
-    public DeputyController Deputy { get => _deputy; set => _deputy = value; }
+    public Character Character { get; private set; }
+    public DeputyController Deputy { get; set; }
 
     private void Awake()
     {
@@ -31,80 +29,38 @@ public class PlayerController : MonoBehaviour, ISavable
             Instance = this;
         }
 
-        _character = GetComponent<Character>();
-        _deputy = FindObjectOfType<DeputyController>();
-    }
-
-    private IEnumerator Interact()
-    {
-        while (_character.IsMoving)
-        {
-            yield return null;
-        }
-
-        Vector3 facingDir = new(_character.Animator.MoveX, _character.Animator.MoveY);
-        Vector3 interactPos = transform.position + facingDir;
-        Collider2D collider = Physics2D.OverlapCircle(interactPos, 0.1f, GameLayers.Instance.InteractablesLayer);
-
-        if (collider != null)
-        {
-            // AudioManager.Instance.PlaySFX(AudioID.Interact);
-            _character.Animator.IsMoving = false;
-            yield return collider.GetComponent<IInteractable>()?.Interact(transform);
-        }
-        _isInteracting = false;
-    }
-
-    private void OnMoveOver()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position - new Vector3(0, _character.OffsetY), 0.2f, GameLayers.Instance.TriggerableLayers);
-        IPlayerTriggerable triggerable = null;
-
-        foreach (Collider2D collider in colliders)
-        {
-            triggerable = collider.GetComponent<IPlayerTriggerable>();
-            if (triggerable != null)
-            {
-                if (triggerable == _currentlyInTrigger && !triggerable.TriggerRepeatedly)
-                {
-                    break;
-                }
-                triggerable.OnPlayerTriggered(this);
-                _currentlyInTrigger = triggerable;
-                break;
-            }
-        }
-
-        if (colliders.Count() == 0 || triggerable != _currentlyInTrigger)
-        {
-            _currentlyInTrigger = null;
-        }
+        Character = GetComponent<Character>();
+        Deputy = FindObjectOfType<DeputyController>();
     }
 
     public void HandleUpdate()
     {
-        if (!_character.IsMoving)
+        if (!Character.IsMoving)
         {
+            // Capture input values.
             _input.x = Input.GetAxisRaw("Horizontal");
             _input.y = Input.GetAxisRaw("Vertical");
 
+            // Prioritize horizontal movement over vertical when both inputs are present.
             if (_input.x != 0)
             {
                 _input.y = 0;
             }
 
+            // Begin movement routine if any input is detected.
             if (_input != Vector2.zero)
             {
-                StartCoroutine(_character.MoveRoutine(_input, OnMoveOver));
+                _ = StartCoroutine(Character.MoveRoutine(_input, OnMoveOver));
             }
         }
 
-        _character.UpdateAnimator();
+        Character.UpdateAnimator();
 
+        // Handle interaction input.
         if (Input.GetButtonDown("Action") && !_isInteracting)
         {
             _isInteracting = true;
-            StartCoroutine(Interact());
+            _ = StartCoroutine(Interact());
         }
     }
 
@@ -112,7 +68,7 @@ public class PlayerController : MonoBehaviour, ISavable
     {
         _playerBattler = battler ?? throw new ArgumentNullException(nameof(battler));
         _playerBattler.IsCommander = true;
-        _name = battler.Base.Name;
+        Name = battler.Base.Name;
 
         BattleParty party = GetComponent<BattleParty>();
         if (!party.Battlers.Contains(battler))
@@ -120,10 +76,10 @@ public class PlayerController : MonoBehaviour, ISavable
             party.AddMember(battler);
         }
 
-        _character.Animator.SetSprites(battler.Base.WalkDownSprites,
-                            battler.Base.WalkUpSprites,
-                            battler.Base.WalkRightSprites,
-                            battler.Base.WalkLeftSprites);
+        Character.Animator.SetSprites(battler.Base.WalkDownSprites,
+                                       battler.Base.WalkUpSprites,
+                                       battler.Base.WalkRightSprites,
+                                       battler.Base.WalkLeftSprites);
     }
 
     public object CaptureState()
@@ -134,7 +90,7 @@ public class PlayerController : MonoBehaviour, ISavable
         PlayerSaveData saveData = new()
         {
             Position = new float[] { transform.position.x, transform.position.y },
-            FacingDirection = _character.Animator.FacingDirection,
+            FacingDirection = Character.Animator.FacingDirection,
             Battlers = party.Select(static b => b.GetSaveData()).ToList(),
             PlayerBattlerIndex = playerIndex
         };
@@ -148,13 +104,65 @@ public class PlayerController : MonoBehaviour, ISavable
         BattleParty party = GetComponent<BattleParty>();
 
         transform.position = new Vector3(saveData.Position[0], saveData.Position[1]);
-        _character.Animator.FacingDirection = saveData.FacingDirection;
+        Character.Animator.FacingDirection = saveData.FacingDirection;
 
         List<Battler> battlers = saveData.Battlers.Select(static s => new Battler(s)).ToList();
-        GetComponent<BattleParty>().Battlers = battlers;
+        party.Battlers = battlers;
 
         SetPlayerBattler(battlers[saveData.PlayerBattlerIndex]);
         party.PartyUpdated();
+    }
+
+    private IEnumerator Interact()
+    {
+        // Wait until the player finishes moving.
+        while (Character.IsMoving)
+        {
+            yield return null;
+        }
+
+        Vector3 facingDir = new(Character.Animator.MoveX, Character.Animator.MoveY);
+        Vector3 interactPos = transform.position + facingDir;
+        Collider2D collider = Physics2D.OverlapCircle(interactPos, 0.1f, GameLayers.Instance.InteractablesLayer);
+
+        if (collider != null)
+        {
+            // AudioManager.Instance.PlaySFX(AudioID.Interact);
+            Character.Animator.IsMoving = false;
+            yield return collider.GetComponent<IInteractable>()?.Interact(transform);
+        }
+        _isInteracting = false;
+    }
+
+    private void OnMoveOver()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(
+            transform.position - new Vector3(0, Character.OffsetY),
+            0.2f,
+            GameLayers.Instance.TriggerableLayers);
+
+        IPlayerTriggerable triggerable = null;
+        foreach (Collider2D collider in colliders)
+        {
+            triggerable = collider.GetComponent<IPlayerTriggerable>();
+            if (triggerable != null)
+            {
+                // If already in this trigger and it doesn't repeat, do nothing.
+                if (triggerable == _currentlyInTrigger && !triggerable.TriggerRepeatedly)
+                {
+                    break;
+                }
+                triggerable.OnPlayerTriggered(this);
+                _currentlyInTrigger = triggerable;
+                break;
+            }
+        }
+
+        // Reset the current trigger if none found or if a different one is active.
+        if (colliders.Length == 0 || triggerable != _currentlyInTrigger)
+        {
+            _currentlyInTrigger = null;
+        }
     }
 }
 
