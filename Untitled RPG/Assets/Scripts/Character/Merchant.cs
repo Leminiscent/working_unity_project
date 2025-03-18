153ffa4274b4db301910c37108b5d4c4b177e09e
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-03-18 12:01:27 -0400

    Refactor Merchant for improved clarity and maintainability; replace private field with a property and update trade logic to use the new property.

--- Untitled RPG/Assets/Scripts/Character/Merchant.cs
+++ Untitled RPG/Assets/Scripts/Character/Merchant.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Merchant : MonoBehaviour
 {
-    [SerializeField] private List<ItemBase> _itemsForSale;
-
-    public List<ItemBase> ItemsForSale => _itemsForSale;
+    [field: SerializeField, FormerlySerializedAs("_itemsForSale")] public List<ItemBase> ItemsForSale { get; private set; }
 
     public IEnumerator Trade()
     {
-        ShopMenuState.Instance.AvailableItems = _itemsForSale;
+        ShopMenuState.Instance.AvailableItems = ItemsForSale;
         yield return GameController.Instance.StateMachine.PushAndWait(ShopMenuState.Instance);
     }
 }
