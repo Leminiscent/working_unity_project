510e1b4b65dc77a3194227283dd30b33256c0065
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2025-04-17 18:53:04 -0400

    Refactor DeputyController to utilize Character component for movement and animation; streamline movement logic and remove unused variables

--- Untitled RPG/Assets/Scripts/Character/DeputyController.cs
+++ Untitled RPG/Assets/Scripts/Character/DeputyController.cs
@@ -2,27 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
 using UnityEngine;
 
 public class DeputyController : MonoBehaviour, ISavable
 {
-    private const float LEDGE_CHECK_RADIUS = 0.15f;
-    private const float JUMP_POWER = 1.42f;
-    private const int NUM_JUMPS = 1;
-    private const float JUMP_DURATION = 0.34f;
-
-    private CharacterAnimator _animator;
-    private bool _isMoving;
+    private Character _character;
     private PlayerController _player;
     private BattleParty _party;
-    private float _moveSpeed;
     private Queue<Vector3> _positionQueue = new();
     private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
-        _animator = GetComponent<CharacterAnimator>();
+        _character = GetComponent<Character>();
         _player = FindObjectOfType<PlayerController>();
         _party = _player.GetComponent<BattleParty>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -30,10 +22,8 @@ public class DeputyController : MonoBehaviour, ISavable
 
     private void Start()
     {
-        _moveSpeed = _player.Character.MoveSpeed;
         _player.Character.OnMoveStart += OnPlayerMoveStart;
         _party.OnUpdated += UpdateDeputyBattler;
-
         UpdateDeputyBattler();
     }
 
@@ -50,11 +40,13 @@ public class DeputyController : MonoBehaviour, ISavable
             return;
         }
 
-        if (!_isMoving && _positionQueue.Count > 0)
+        if (!_character.IsMoving && _positionQueue.Count > 0)
         {
-            Vector3 targetPosition = _positionQueue.Dequeue();
-            _ = StartCoroutine(MoveToPosition(targetPosition));
+            Vector3 targetPos = _positionQueue.Dequeue();
+            _ = StartCoroutine(FollowRoutine(targetPos));
         }
+
+        _character.UpdateAnimator();
     }
 
     public void UpdateDeputyBattler()
@@ -64,15 +56,12 @@ public class DeputyController : MonoBehaviour, ISavable
             SetSpriteVisibility(false);
             return;
         }
-        else
-        {
-            SetSpriteVisibility(true);
-        }
 
-        // Get the first battler who is not the commander.
-        Battler deputy = _party.Battlers.First(static battler => !battler.IsCommander);
+        SetSpriteVisibility(true);
+        Battler deputy = _party.Battlers.First(static b => !b.IsCommander);
 
-        _animator.SetSprites(
+        // Set the new Deputy's sprites
+        _character.Animator.SetSprites(
             deputy.Base.WalkDownSprites,
             deputy.Base.WalkUpSprites,
             deputy.Base.WalkRightSprites,
@@ -98,92 +87,51 @@ public class DeputyController : MonoBehaviour, ISavable
             SetSpriteVisibility(false);
             return;
         }
-        else
-        {
-            SetSpriteVisibility(true);
-        }
 
-        transform.position = _player.transform.position;
-        _animator.IsMoving = false;
-        _isMoving = false;
+        SetSpriteVisibility(true);
+
+        // Snap to the player’s tile
+        _character.SnapToTile(_player.transform.position);
+        _character.Animator.IsMoving = false;
     }
 
     public object CaptureState()
     {
-        DeputySaveData saveData = new()
+        return new DeputySaveData
         {
             Position = new float[] { transform.position.x, transform.position.y },
-            FacingDirection = _animator.FacingDirection,
+            FacingDirection = _character.Animator.FacingDirection
         };
-        return saveData;
     }
 
     public void RestoreState(object state)
     {
         DeputySaveData saveData = (DeputySaveData)state;
-        transform.position = new Vector3(saveData.Position[0], saveData.Position[1]);
-        _animator.FacingDirection = saveData.FacingDirection;
+        _character.SnapToTile(new Vector2(saveData.Position[0], saveData.Position[1]));
+        _character.Animator.FacingDirection = saveData.FacingDirection;
     }
 
-    private IEnumerator MoveToPosition(Vector3 targetPos)
+    private IEnumerator FollowRoutine(Vector3 targetPos)
     {
-        _isMoving = true;
-        _animator.IsMoving = true;
-
+        // Step one tile at a time until we reach the dequeue’d position
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            Vector2 moveVec = GetNextMoveVector(transform.position, targetPos);
+            Vector2Int currentGrid = new(
+                Mathf.FloorToInt(transform.position.x),
+                Mathf.FloorToInt(transform.position.y));
+            Vector2Int targetGrid = new(
+                Mathf.FloorToInt(targetPos.x),
+                Mathf.FloorToInt(targetPos.y));
+            Vector2Int diff = targetGrid - currentGrid;
 
-            _animator.MoveX = moveVec.x;
-            _animator.MoveY = moveVec.y;
+            // Normalize to a single-axis step
+            Vector2 moveVec = diff.x != 0
+                ? new Vector2(Mathf.Sign(diff.x), 0)
+                : new Vector2(0, Mathf.Sign(diff.y));
 
-            Vector3 nextPos = transform.position + (Vector3)moveVec;
-            Ledge ledge = CheckForLedge(nextPos);
-
-            if (ledge != null)
-            {
-                if (ledge.CanJump(moveVec))
-                {
-                    Vector3 jumpDest = transform.position + ((Vector3)moveVec * 2);
-                    yield return Jump(jumpDest);
-                    continue;
-                }
-            }
-
-            while ((nextPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, nextPos, _moveSpeed * Time.deltaTime);
-                yield return null;
-            }
-
-            transform.position = nextPos;
+            // Hand off to Character.MoveRoutine for all collisions/ledge jumps/etc.
+            yield return StartCoroutine(_character.MoveRoutine(moveVec));
         }
-
-        _animator.IsMoving = false;
-        _isMoving = false;
-    }
-
-    private Vector2 GetNextMoveVector(Vector3 fromPosition, Vector3 toPosition)
-    {
-        Vector2 direction = toPosition - fromPosition;
-        return Mathf.Abs(direction.x) > Mathf.Epsilon
-            ? new Vector2(Mathf.Sign(direction.x), 0)
-            : Mathf.Abs(direction.y) > Mathf.Epsilon ? new Vector2(0, Mathf.Sign(direction.y)) : Vector2.zero;
-    }
-
-    private Ledge CheckForLedge(Vector3 targetPos)
-    {
-        Collider2D collider = Physics2D.OverlapCircle(targetPos, LEDGE_CHECK_RADIUS, GameLayers.Instance.LedgeLayer);
-        return collider != null ? collider.GetComponent<Ledge>() : null;
-    }
-
-    private IEnumerator Jump(Vector3 jumpDest)
-    {
-        _isMoving = true;
-        _animator.IsJumping = true;
-        yield return transform.DOJump(jumpDest, JUMP_POWER, NUM_JUMPS, JUMP_DURATION).WaitForCompletion();
-        _animator.IsJumping = false;
-        _isMoving = false;
     }
 
     private void SetSpriteVisibility(bool visible)
